cb7e7ee9fa47ded21b982d08b521a4a3ec6bb54d
Chandru-trigeminal <chandramohan@trigeminal-ai.com>
2024-12-28 00:29:55 +0530

    added new components

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const TaskForm = () => {
+
+    const [task, setTask] = useState('');
+
+    const [priority, setPriority] = useState('Medium');;
+
+    const [category, setCategory] = useState('General');
+
   return (
-    <div>Taskform</div>
+    <form>
+        <div id='inp'>
+            <input type="text"
+            placeholder='Enter your task'
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            required
+            />
+            <span><button type='submit'>Add Task</button></span>
+            <h1>{task}</h1>
+        </div>
+        <div>
+            <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+                <option value="High"></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select>
+        </div>
+    </form>
   )
 }
 
