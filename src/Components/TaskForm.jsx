4ca017f1e9568c1e83e5b4143c2ab5d93fa5ed6f
Chandru-trigeminal <chandramohan@trigeminal-ai.com>
2024-12-28 07:05:45 +0530

    added fields

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -1,34 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const TaskForm = () => {
+const TaskForm = ({addTask}) => {
+  const [task, setTask] = useState("");
 
-    const [task, setTask] = useState('');
+  const [priority, setPriority] = useState("Medium");
 
-    const [priority, setPriority] = useState('Medium');;
+  const [category, setCategory] = useState("General");
 
-    const [category, setCategory] = useState('General');
+  const handleSubmit = () => {
+    addTask({text: task, priority, category, completed: false});
+  }
 
   return (
-    <form>
-        <div id='inp'>
-            <input type="text"
-            placeholder='Enter your task'
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-            required
-            />
-            <span><button type='submit'>Add Task</button></span>
-            <h1>{task}</h1>
-        </div>
-        <div>
-            <select value={priority} onChange={(e) => setPriority(e.target.value)}>
-                <option value="High"></option>
-                <option value=""></option>
-                <option value=""></option>
-            </select>
-        </div>
+    <form onSubmit={handleSubmit}>
+      <div id="inp">
+        <input
+          type="text"
+          placeholder="Enter your task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          required
+        />
+        <span>
+          <button type="submit">Add Task</button>
+        </span>
+        <h1>{task}</h1>
+      </div>
+      <div>
+        <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+          <option value="High">High</option>
+          <option value="Medium">Medium</option>
+          <option value="Low">Low</option>
+        </select>
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="General">General</option>
+          <option value="Work">work</option>
+          <option value="Personal">Personal</option>
+        </select>
+      </div>
     </form>
-  )
-}
+  );
+};
 
 export default TaskForm;
