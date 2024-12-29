97553a17857242c66b1c707f45c766f27d591e98
Chandru-trigeminal <chandramohan@trigeminal-ai.com>
2024-12-29 11:58:01 +0530

    final-code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import TaskForm from "./Components/TaskForm.jsx";
 import "./Style.css";
+import Tasklist from "./Components/Tasklist.jsx";
+import ProgressTracker from "./Components/ProgressTracker.jsx";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  
+  
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks)); // to converted to the string
   }, [tasks]);
@@ -13,8 +17,22 @@ function App() {
     setTasks([...tasks, task]);
   };
 
+  const updateTask = (index, updatedTask) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = updatedTask;
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index)); // absence of first argument
+  };
+
+  const clearTasks = () => {
+    setTasks([]);
+  };
+
   return (
-    <div className='App'>
+    <div className="App">
       <header class="header">
         <div class="header-container">
           <h1 class="title">
@@ -24,6 +42,13 @@ function App() {
         </div>
       </header>
       <TaskForm addTask={addTask} />
+      <Tasklist tasks={tasks} updateTask={updateTask} deleteTask={deleteTask} />
+      <ProgressTracker tasks={tasks} />
+      {tasks.length > 0 && (
+        <button className="clear-btn" onClick={clearTasks}>
+          Clear All Tasks
+        </button>
+      )}
     </div>
   );
 }
--- src/Components/ProgressTracker.jsx
+++ src/Components/ProgressTracker.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const ProgressTracker = () => {
+const ProgressTracker = ({ tasks }) => {
+  const completeTasks = tasks.filter((task) => task.completed).length;
+
+  const totalTasks = tasks.length;
+
+  const progress = totalTasks === 0 ? 0 : (completeTasks / totalTasks) * 100;
+
   return (
-    <div>ProgressTracker</div>
-  )
-}
+    <div className="progress-tracker">
+      <p>
+        {completeTasks} of {totalTasks} tasks completed
+      </p>
+      <div className="progress-bar">
+        <div className="progress" style={{ width: `${progress}%` }}></div>
+      </div>
+    </div>
+  );
+};
 
-export default ProgressTracker
+export default ProgressTracker;
